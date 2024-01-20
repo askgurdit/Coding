20e27177f9865e4aa528b0ae847b9a93f6a4f767
Gurdit Singh <118066728+askgurdit@users.noreply.github.com>
2024-01-20 15:02:42 +0530

    Update loops.js

--- Java Script/loops.js
+++ Java Script/loops.js
@@ -1,10 +1,10 @@
-/*for (let i=1; i>=0; i++) {
+for (let i=1; i>=0; i++) {
     console.log ("i=",i);
-} //Never ending Infinite loop*/
+} //Never ending Infinite loop
 /*for (let i=0; i<=100; i++) {
     console.log ("i=",i);
-} // For loop for printing the numbers 0 to 100 */
+} // For loop for printing the numbers 0 to 100 
 for (let num=0; num<=100; num++) {
     if(num%2===0)
 console.log ("num=",num);
-}
+} // To print even numbers*/
